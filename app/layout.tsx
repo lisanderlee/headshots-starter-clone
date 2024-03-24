921bb4b699a832c6d05fcda0892afba69a504a4b
Lisandro <lisander.lee@gmail.com>
2024-03-23 21:15:10 -0700

    fixed hydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
@@ -25,18 +26,14 @@ export default function RootLayout({ children }: any) {
         />
       </head>
       <body className="min-h-screen flex flex-col">
-     
+   
         <section>
           <Suspense
             fallback={
               <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
             }
           >
-            <Navbar />
-            
-          </Suspense>
-        </section>
-          <Script
+               <Script
             async
             src="https://cdn.snipcart.com/themes/v3.0.31/default/snipcart.js"
           />
@@ -46,13 +43,18 @@ export default function RootLayout({ children }: any) {
             data-api-key={process.env.NEXT_PUBLIC_SNIPCART_API_KEY}
             hidden
           >
-            <address-fields section="top">
+    
               <div className="snipcart-form__field">
                 <snipcart-label for="phone">Phone number</snipcart-label>
                 <snipcart-input name="phone"></snipcart-input>
               </div>
-            </address-fields>
+         
           </div>
+            <Navbar />
+            
+          </Suspense>
+        </section>
+     
         <main className="flex flex-1 flex-col items-center py-16">
       
           {children}
