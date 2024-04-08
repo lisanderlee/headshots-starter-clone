8c83f015cb5aa947f7f8798f92d691c0012e6fe6
Lisandro <lisander.lee@gmail.com>
2024-04-08 14:32:36 -0700

    shipping test

--- app/api/snipcart/shipping/route.ts
+++ app/api/snipcart/shipping/route.ts
@@ -25,66 +25,71 @@ type Error = {
   errors: { key: string; message: string }[];
 };
 
-export async function POST(   request: Request,) {
+export async function POST(   req: Request, res:Response) {
     /* @ts-ignore */
   const { eventName, content } = request.body;
+ 
 
-  console.log("ENTRAA", eventName, content, request.body)
+  /* @ts-ignore */
+  if (eventName !== "shippingrates.fetch") return res.status(200).end();
+    /* @ts-ignore */
+  if (content.items.length === 0) return res.status(200).end();
 
-  // if (eventName !== "shippingrates.fetch") return res.status(200).end();
-  // if (content.items.length === 0) return res.status(200).end();
+  const {
+    items: cartItems,
+    shippingAddress1,
+    shippingAddress2,
+    shippingAddressCity,
+    shippingAddressCountry,
+    shippingAddressProvince,
+    shippingAddressPostalCode,
+    shippingAddressPhone,
+  } = content;
 
-  // const {
-  //   items: cartItems,
-  //   shippingAddress1,
-  //   shippingAddress2,
-  //   shippingAddressCity,
-  //   shippingAddressCountry,
-  //   shippingAddressProvince,
-  //   shippingAddressPostalCode,
-  //   shippingAddressPhone,
-  // } = content;
+  const recipient = {
+    ...(shippingAddress1 && { address1: shippingAddress1 }),
+    ...(shippingAddress2 && { address2: shippingAddress2 }),
+    ...(shippingAddressCity && { city: shippingAddressCity }),
+    ...(shippingAddressCountry && { country_code: shippingAddressCountry }),
+    ...(shippingAddressProvince && { state_code: shippingAddressProvince }),
+    ...(shippingAddressPostalCode && { zip: shippingAddressPostalCode }),
+    ...(shippingAddressPhone && { phone: shippingAddressPhone }),
+  };
 
-  // const recipient = {
-  //   ...(shippingAddress1 && { address1: shippingAddress1 }),
-  //   ...(shippingAddress2 && { address2: shippingAddress2 }),
-  //   ...(shippingAddressCity && { city: shippingAddressCity }),
-  //   ...(shippingAddressCountry && { country_code: shippingAddressCountry }),
-  //   ...(shippingAddressProvince && { state_code: shippingAddressProvince }),
-  //   ...(shippingAddressPostalCode && { zip: shippingAddressPostalCode }),
-  //   ...(shippingAddressPhone && { phone: shippingAddressPhone }),
-  // };
+  const items: PrintfulShippingItem[] = cartItems.map(
+      /* @ts-ignore */
+    (item): PrintfulShippingItem => ({
+      external_variant_id: item.id,
+      quantity: item.quantity,
+    })
+  );
 
-  // const items: PrintfulShippingItem[] = cartItems.map(
-  //   (item): PrintfulShippingItem => ({
-  //     external_variant_id: item.id,
-  //     quantity: item.quantity,
-  //   })
-  // );
-
-  // try {
-  //   const { result } = await printful.post("shipping/rates", {
-  //     recipient,
-  //     items,
-  //   });
-
-  //   res.status(200).json({
-  //     rates: result.map((rate) => ({
-  //       cost: rate.rate,
-  //       description: rate.name,
-  //       userDefinedId: rate.id,
-  //       guaranteedDaysToDelivery: rate.maxDeliveryDays,
-  //     })),
-  //   });
-  // } catch ({ error }) {
-  //   console.log(error);
-  //   res.status(200).json({
-  //     errors: [
-  //       {
-  //         key: error?.reason,
-  //         message: error?.message,
-  //       },
-  //     ],
-  //   });
-  // }
+  try {
+    const { result } = await printful.post("shipping/rates", {
+      recipient,
+      items,
+    });
+  /* @ts-ignore */
+    res.status(200).json({
+        /* @ts-ignore */
+      rates: result.map((rate) => ({
+        cost: rate.rate,
+        description: rate.name,
+        userDefinedId: rate.id,
+        guaranteedDaysToDelivery: rate.maxDeliveryDays,
+      })),
+    });
+      /* @ts-ignore */
+  } catch ({ error }) {
+    console.log(error);
+      /* @ts-ignore */
+    res.status(200).json({
+      errors: [
+        {
+          key: error?.reason,
+          message: error?.message,
+        },
+      ],
+    });
+  }
 }
