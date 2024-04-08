52aea3dbdbb928f587415fe76b0a214629dbd0fb
Lisandro <lisander.lee@gmail.com>
2024-04-07 19:31:02 -0700

    test new shopping

--- app/api/snipcart/shipping/route.ts
+++ app/api/snipcart/shipping/route.ts
@@ -1,17 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from 'next/server';
 import { printful } from "@/components/shop/lib/printful-client";
 import type {
   SnipcartShippingRate,
   PrintfulShippingItem,
 } from "@/types/printful";
 
-
 type SnipcartRequest = {
-  body: {
-    eventName: string;
-    mode: string;
-    createdOn: string;
-    content: { [key: string]: any };
+  eventName: string;
+  content: {
+    items: {
+      id: string;
+      quantity: number;
+    }[];
+    shippingAddress1?: string;
+    shippingAddress2?: string;
+    shippingAddressCity?: string;
+    shippingAddressCountry?: string;
+    shippingAddressProvince?: string;
+    shippingAddressPostalCode?: string;
+    shippingAddressPhone?: string;
   };
 };
 
@@ -23,15 +30,17 @@ type Data = {
 type Error = {
   errors: { key: string; message: string }[];
 };
+  /* @ts-ignore */
+export async function POST(request) {
+  const { eventName, content } = await request.json();
 
-export default async function handler(
-  req: SnipcartRequest,
-  res: NextApiResponse<Data | Error>
-) {
-  const { eventName, content } = req.body;
+  if (eventName !== 'shippingrates.fetch') {
+    return new NextResponse(null, { status: 200 });
+  }
 
-  if (eventName !== "shippingrates.fetch") return res.status(200).end();
-  if (content.items.length === 0) return res.status(200).end();
+  if (content.items.length === 0) {
+    return new NextResponse(null, { status: 200 });
+  }
 
   const {
     items: cartItems,
@@ -55,39 +64,36 @@ export default async function handler(
   };
 
   const items: PrintfulShippingItem[] = cartItems.map(
-      /* @ts-ignore */
-    (item): PrintfulShippingItem => ({
+    (item: any): PrintfulShippingItem => ({
       external_variant_id: item.id,
       quantity: item.quantity,
     })
   );
 
   try {
-    const { result } = await printful.post("shipping/rates", {
+    const { result } = await printful.post('shipping/rates', {
       recipient,
       items,
     });
 
-    res.status(200).json({
-        /* @ts-ignore */
-      rates: result.map((rate) => ({
+    return new NextResponse(JSON.stringify({
+      rates: result.map((rate: any) => ({
         cost: rate.rate,
         description: rate.name,
         userDefinedId: rate.id,
         guaranteedDaysToDelivery: rate.maxDeliveryDays,
-      })),
-    });
+      }))
+    }), { status: 200 });
       /* @ts-ignore */
   } catch ({ error }) {
-    
     console.log(error);
-    res.status(200).json({
+    return new NextResponse(JSON.stringify({
       errors: [
         {
           key: error?.reason,
           message: error?.message,
         },
       ],
-    });
+    }), { status: 200 });
   }
 }
