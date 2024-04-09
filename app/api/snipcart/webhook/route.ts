607dcb98c325124d471439b4e103475e99fb20db
Lisandro <lisander.lee@gmail.com>
2024-04-09 13:27:19 -0700

    test 2

--- app/api/snipcart/webhook/route.ts
+++ app/api/snipcart/webhook/route.ts
@@ -1,6 +1,5 @@
 import type { SnipcartRequest, SnipcartWebhookEvent } from "@/types/printful";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import type { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import createOrder from "@/components/shop/lib/create-order";
 
@@ -14,18 +13,19 @@ export async function POST(req: NextRequest) {
   const result = await req.json();
   const eventName = result.eventName;
   const content = result.content;
-  const method = req.method;
+  const method = req.method
+
+
 
   if (method !== "POST")
     /* @ts-ignore */
-    return NextResponse.status(405).json({ message: "Method not allowed" });
+    return res.status(405).json({ message: "Method not allowed" });
 
   if (!allowedEvents.includes(eventName))
-       /* @ts-ignore */
-    return NextResponse.status(400).json({ message: "This event is not permitted" });
-
-   /* @ts-ignore */
-  if (!token) return  NextResponse.status(401).json({ message: "Not Authorized" });
+    /* @ts-ignore */
+    return res.status(400).json({ message: "This event is not permitted" });
+  /* @ts-ignore */
+  if (!token) return res.status(401).json({ message: "Not Authorized" });
 
   try {
     const verifyToken = await fetch(
@@ -33,18 +33,16 @@ export async function POST(req: NextRequest) {
     );
 
     if (!verifyToken.ok)
-      /* @ts-ignore */
-      return NextResponse.status(401).json({ message: "Not Authorization" });
+        /* @ts-ignore */
+      return res.status(401).json({ message: "Not Authorization" });
   } catch (err) {
     console.log(err);
-    return (
-      NextResponse
-        /* @ts-ignore */
-        .status(500)
-        .json({ message: "Unable to verify Snipcart webhook token" })
-    );
+      /* @ts-ignore */
+    return res
+      .status(500)
+      .json({ message: "Unable to verify Snipcart webhook token" });
   }
-
+  
   try {
     switch (eventName) {
       case "order.completed":
@@ -52,12 +50,9 @@ export async function POST(req: NextRequest) {
         break;
       case "customauth:customer_updated":
         /* @ts-ignore */
-        return (
-          NextResponse
-            /* @ts-ignore */
-            .status(200)
-            .json({ message: "Customer updated - no action taken" })
-        );
+        return res
+          .status(200)
+          .json({ message: "Customer updated - no action taken" });
       default:
         throw new Error("No such event handler exists");
     }
