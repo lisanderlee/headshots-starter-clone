b1b9ed7f2be0e419cce6ff27c34b108919cbbe3c
Lisandro <lisander.lee@gmail.com>
2024-04-09 12:25:04 -0700

    test 2

--- app/api/snipcart/webhook/route.ts
+++ app/api/snipcart/webhook/route.ts
@@ -1,66 +1,25 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { SnipcartRequest, SnipcartWebhookEvent } from "@/types/printful";
+import createOrder from "@/components/shop/lib/create-order";
 
 export async function POST(req: Request) {
+
+  const allowedEvents: SnipcartWebhookEvent[] = [
+    "order.completed",
+    "customauth:customer_updated",
+  ];
+
   const result = await req.json();
+  console.log("HEADERS", result.headers);
   const eventName = result.eventName;
   const content = result.content;
-  console.log("EVENT", eventName);
-  console.log("CONTENT", content);
+
+
+
+
+
+
+
+
 }
 
-// import type { SnipcartRequest, SnipcartWebhookEvent } from "@/types/printful";
 
-// import createOrder from "@/components/shop/lib/create-order";
-
-// export async function POST(req: NextRequest) {
-//   const allowedEvents: SnipcartWebhookEvent[] = [
-//     "order.completed",
-//     "customauth:customer_updated",
-//   ];
-
-//   const { headers, body } = req;
-//   /* @ts-ignore */
-//   // if (headers.get("content-type") !== "application/json") {
-//   //   return new NextResponse("Content-Type must be application/json", {
-//   //     status: 400,
-//   //   });
-//   // }
-//   /* @ts-ignore */
-//   const token = headers.get("x-snipcart-requesttoken");
-//   /* @ts-ignore */
-//   const { eventName, content } = body;
-
-//   if (eventName !== "order.completed" && eventName !== "customauth:customer_updated") {
-//     return new NextResponse("This event is not permitted", { status: 400 });
-//   }
-
-//   if (!token) {
-//     return new NextResponse("Not Authorized", { status: 401 });
-//   }
-
-//   try {
-//     const response = await fetch(
-//       `https://app.snipcart.com/api/requestvalidation/${token}`
-//     );
-
-//     if (!response.ok) {
-//       return new NextResponse("Not Authorized", { status: 401 });
-//     }
-
-//     switch (eventName) {
-//       case "order.completed":
-//         await createOrder(content);
-//         return new NextResponse("Order created", { status: 200 });
-//         break;
-//       case "customauth:customer_updated":
-//         return new NextResponse("Customer updated", { status: 200 });
-//         break;
-//       default:
-//         throw new Error("No such event handler exists");
-//     }
-//   } catch (err) {
-//     console.log(err);
-
-//     return new NextResponse("Something went wrong", { status: 500 });
-//   }
-// }
